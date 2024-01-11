d70f12c3de215d77101f8e0bb3c594a1a74b55cf
Mindy D Tran <mindytran98@gmail.com>
2024-01-11 10:06:48 -0500

    dummy data for users

---
+++ data/users.js
@@ -0,0 +1,34 @@
+const users = [
+    {
+        id:1,
+        name: 'Scien',
+        username: 'ScienTheScienceGuy',
+        email: 'scienTheScienceGuy@example.com'
+    },
+    {
+        id:2,
+        name: 'Takeru',
+        username: 'donutBoy25',
+        email: 'sTakeru@example.com'
+    },
+    {
+        id:3,
+        name: 'Jupiter',
+        username: 'peter_flag',
+        email: 'peter_flag@example.com'
+    },
+    {
+        id:4,
+        name: 'HiMERU',
+        username: 'official_HiMERU',
+        email: 'theNumber1IdolHiMERU@example.com'
+    },
+    {
+        id:5,
+        name: 'Itaru',
+        username: 'taruchi',
+        email: 'taruchiBusiness@example.com'
+    }
+];
+
+module.exports = users;
