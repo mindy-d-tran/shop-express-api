6e1df9a09016fd6cce795f9d449c3f86c7931197
Mindy D Tran <mindytran98@gmail.com>
2024-01-11 11:14:31 -0500

    create dummy data for data

---
+++ data/products.js
@@ -0,0 +1,34 @@
+const products = [
+    {
+        id: 1,
+        listingName: 'washi tape',
+        price: 4,
+        img: ''
+    },
+    {
+        id: 2,
+        listingName: 'hardcover notebook',
+        price: 10,
+        img: ''
+    },
+    {
+        id: 3,
+        listingName: 'sticker sheet',
+        price: 6,
+        img: ''
+    },
+    {
+        id: 4,
+        listingName: 'pen (1 count)',
+        price: 3,
+        img: ''
+    },
+    {
+        id: 5,
+        listingName: 'highlighter (5 count)',
+        price: 10,
+        img: ''
+    }
+];
+
+module.exports = products;
