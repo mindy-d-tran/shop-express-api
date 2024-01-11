c2777160a6657207362a493336e45637184303bf
Mindy D Tran <mindytran98@gmail.com>
2024-01-11 10:22:12 -0500

    create custom middleware for logging requests

--- index.js
+++ index.js
@@ -5,51 +5,66 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+// importing routes
+const users = require('./routes/users');
+
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ extended: true }));
 
-app.get('/', (req,res)=>{
-    res.send('i am alive!')
+// middleware
+app.use((req,res,next)=>{
+    const time = new Date();
+    console.log(`${time.toLocaleTimeString()}: Received a ${req.method} request to ${req.url}.`);
+    if(!Object.keys(req.body).length){
+        console.log('Data:');
+        console.log(`${JSON.stringify(req.body)}`);
+    }
+    next();
 })
 
 app.get("/api", (req, res) => {
-  res.json({
-    links: [
-      {
-        href: "/api/users",
-        rel: "users",
-        type: "GET",
-      },
-      {
-        href: "/api/users",
-        rel: "users",
-        type: "POST",
-      },
-      {
-        href: "/api/products",
-        rel: "products",
-        type: "GET",
-      },
-      {
-        href: "/api/products",
-        rel: "products",
-        type: "POST",
-      },
-      {
-        href: "/api/orders",
-        rel: "orders",
-        type: "GET",
-      },
-      {
-        href: "/api/orders",
-        rel: "orders",
-        type: "POST",
-      }
-    ]
-  });
+    res.json({
+        links: [
+            {
+                href: "/api/users",
+                rel: "users",
+                type: "GET",
+            },
+            {
+                href: "/api/users",
+                rel: "users",
+                type: "POST",
+            },
+            {
+                href: "/api/products",
+                rel: "products",
+                type: "GET",
+            },
+            {
+                href: "/api/products",
+                rel: "products",
+                type: "POST",
+            },
+            {
+                href: "/api/orders",
+                rel: "orders",
+                type: "GET",
+            },
+            {
+                href: "/api/orders",
+                rel: "orders",
+                type: "POST",
+            }
+        ]
+    });
 });
 
+app.use('/api/users', users);
+
+app.get('/', (req,res)=>{
+    res.send('i am alive!')
+})
 app.listen(port, () => console.log("server port: " + port));
