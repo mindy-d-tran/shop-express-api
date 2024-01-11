b699e851858e745af39b64916f9922d32a850d26
Mindy D Tran <mindytran98@gmail.com>
2024-01-11 11:57:50 -0500

    import dummy data

--- routes/orders.js
+++ routes/orders.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const orders = require('../data/orders');
+
+router.route('/').get((req,res)=>{res.json(orders)});
+
 module.exports = router;
--- routes/products.js
+++ routes/products.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const products = require('../data/products');
+
+router.route('/').get((req,res)=>{
+    res.json(products);
+})
+
 module.exports = router;
