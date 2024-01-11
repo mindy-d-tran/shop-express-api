36a22c501c085930f0a067bd1751f78ec954d89c
Mindy D Tran <mindytran98@gmail.com>
2024-01-11 09:54:18 -0500

    created link for api

--- index.js
+++ index.js
@@ -1,8 +1,55 @@
-const express = require('express');
+/**
+ * Project: create an express api
+ * users, products, order
+ */
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req,res)=>{ res.send('i am alive!')});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ extended: true }));
 
-app.listen(port, ()=> console.log('server port: ' + port))
+app.get('/', (req,res)=>{
+    res.send('i am alive!')
+})
+
+app.get("/api", (req, res) => {
+  res.json({
+    links: [
+      {
+        href: "/api/users",
+        rel: "users",
+        type: "GET",
+      },
+      {
+        href: "/api/users",
+        rel: "users",
+        type: "POST",
+      },
+      {
+        href: "/api/products",
+        rel: "products",
+        type: "GET",
+      },
+      {
+        href: "/api/products",
+        rel: "products",
+        type: "POST",
+      },
+      {
+        href: "/api/orders",
+        rel: "orders",
+        type: "GET",
+      },
+      {
+        href: "/api/orders",
+        rel: "orders",
+        type: "POST",
+      }
+    ]
+  });
+});
+
+app.listen(port, () => console.log("server port: " + port));
