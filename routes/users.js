4f561f1185cc36346d5284b578dd7f01ede18e47
Mindy D Tran <mindytran98@gmail.com>
2024-01-11 20:02:15 -0500

    add post method

--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,31 @@ const router = express.Router();
 
 const users = require("../data/users");
 
-router.route("/").get((req, res) => {
-  res.json(users);
-});
+router
+  .route("/")
+  .get((req, res) => {
+    res.json(users);
+  })
+  .post((req, res) => {
+    if (req.body.name && req.body.username && req.body.email) {
+      if (users.find((u) => u.username == req.body.username)) {
+        res.json({ error: "username already taken" });
+        return;
+      }
+      if (users.find((e) => e.email == req.body.email)) {
+        res.json({ error: "email already taken" });
+        return;
+      }
+      const user = {
+        id: users.length+1,
+        name: req.body.name,
+        username: req.body.username,
+        email: req.body.email,
+      };
+
+      users.push(user);
+      res.json(user);
+    } else res.json({error: "not enough data"});
+  });
 
 module.exports = router;
