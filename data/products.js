2bd94331dd2e678723d11102f401a090b1fe451d
Mindy D Tran <mindytran98@gmail.com>
2024-01-12 22:31:45 -0500

    change property name from img to imgSrc

--- data/products.js
+++ data/products.js
@@ -4,31 +4,31 @@ const products = [
         id: 1,
         listingName: 'washi tape',
         price: 4,
-        img: ''
+        imgSrc: ''
     },
     {
         id: 2,
         listingName: 'hardcover notebook',
         price: 10,
-        img: ''
+        imgSrc: ''
     },
     {
         id: 3,
         listingName: 'sticker sheet',
         price: 6,
-        img: ''
+        imgSrc: ''
     },
     {
         id: 4,
         listingName: 'pen (1 count)',
         price: 3,
-        img: ''
+        imgSrc: ''
     },
     {
         id: 5,
         listingName: 'highlighter (5 count)',
         price: 10,
-        img: ''
+        imgSrc: ''
     }
 ];
 
