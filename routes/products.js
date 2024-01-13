06036a77ea0d7b81feba9edb9b04ead2b0e75b31
Mindy D Tran <mindytran98@gmail.com>
2024-01-12 23:09:18 -0500

    add delete and patch

--- routes/products.js
+++ routes/products.js
@@ -1,21 +1,54 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const products = require('../data/products');
+const products = require("../data/products");
 
-router.route('/').get((req,res)=>{
+router
+  .route("/")
+  .get((req, res) => {
     res.json(products);
-}).post((req,res)=>{
-    if(req.body.listingName && req.body.price && req.body.imgSrc){
-        const product = {
-            id: products.length+1,
-            listingName: req.body.listingName,
-            price: req.body.price,
-            imgSrc: req.body.imgSrc
-        };
-        products.push(product);
-        res.json(product);
-    } else res.json({error: "not enough data"});
-})
+  })
+  .post((req, res) => {
+    if (req.body.listingName && req.body.price && req.body.imgSrc) {
+      const product = {
+        id: products.length + 1,
+        listingName: req.body.listingName,
+        price: req.body.price,
+        imgSrc: req.body.imgSrc,
+      };
+      products.push(product);
+      res.json(product);
+    } else res.json({ error: "not enough data" });
+  });
 
+router
+  .route("/:id")
+  .get((req, res) => {
+    const product = products.find((p) => p.id == req.params.id);
+    if (product) res.json(product);
+    else next();
+  })
+  .patch((req, res, next) => {
+    const product = products.find((p, i) => {
+      if (p.id == req.params.id) {
+        for (const key in req.body) {
+          products[i][key] = req.body[key];
+        }
+        return true;
+      }
+    });
+    if (product) res.json(product);
+    else next();
+  })
+  .delete((req, res, next) => {
+    const product = products.find((p, i) => {
+        if (p.id == req.params.id) {
+          products.splice(i,1);
+          return true;
+        }
+      });
+      if (product) res.json(product);
+      else next();
+  });
+  
 module.exports = router;
