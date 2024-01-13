2614a529c56235b2c30bda21ab1f7685b1117cff
Mindy D Tran <mindytran98@gmail.com>
2024-01-12 21:14:53 -0500

    remove style property

--- data/products.js
+++ data/products.js
@@ -3,28 +3,24 @@ const products = [
     {
         id: 1,
         listingName: 'washi tape',
-        styles: [],
         price: 4,
         img: ''
     },
     {
         id: 2,
         listingName: 'hardcover notebook',
-        styles: [],
         price: 10,
         img: ''
     },
     {
         id: 3,
         listingName: 'sticker sheet',
-        styles: [],
         price: 6,
         img: ''
     },
     {
         id: 4,
         listingName: 'pen (1 count)',
-        styles: [],
         price: 3,
         img: ''
     },
