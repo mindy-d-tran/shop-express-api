e14e2d95dc9517115852b8871dd134279e973825
Mindy D Tran <mindytran98@gmail.com>
2024-01-12 22:38:11 -0500

    add post method

--- routes/products.js
+++ routes/products.js
@@ -5,6 +5,17 @@ const products = require('../data/products');
 
 router.route('/').get((req,res)=>{
     res.json(products);
+}).post((req,res)=>{
+    if(req.body.listingName && req.body.price && req.body.imgSrc){
+        const product = {
+            id: products.length+1,
+            listingName: req.body.listingName,
+            price: req.body.price,
+            imgSrc: req.body.imgSrc
+        };
+        products.push(product);
+        res.json(product);
+    } else res.json({error: "not enough data"});
 })
 
 module.exports = router;
