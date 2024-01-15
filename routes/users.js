0d15bf7bd7cd5fdea39cc44b1812b15705630f2f
Mindy D Tran <mindytran98@gmail.com>
2024-01-15 17:42:32 -0500

    successfully render table

--- routes/users.js
+++ routes/users.js
@@ -2,16 +2,26 @@ const express = require("express");
 const router = express.Router();
 
 const users = require("../data/users");
-router.use(express.static('./styles'));
+router.use(express.static("./styles"));
 
 router
   .route("/")
   .get((req, res) => {
-    const options={
-      title: 'Users'
-    }
-    res.render('users', options);
-    // res.json(users);
+    let test = "";
+    users.forEach((key) => {
+      test+=(`<tr>
+            <td>${key.id}</td>
+            <td>${key.name}</td>
+            <td>${key.username}</td>
+            <td>${key.email}</td>
+        </tr>`);
+    });
+    // console.log(test);
+    const options = {
+      title: "Users",
+      tableRow: test,
+    };
+    res.render("users", options);
   })
   .post((req, res) => {
     if (req.body.name && req.body.username && req.body.email) {
@@ -24,7 +34,7 @@ router
         return;
       }
       const user = {
-        id: users.length+1,
+        id: users.length + 1,
         name: req.body.name,
         username: req.body.username,
         email: req.body.email,
@@ -32,38 +42,39 @@ router
 
       users.push(user);
       res.json(user);
-    } else res.json({error: "not enough data"});
-});
+    } else res.json({ error: "not enough data" });
+  });
 
 router
-  .route('/:id')
-  .get((req,res, next)=>{
+  .route("/:id")
+  .get((req, res, next) => {
     const user = users.find((u) => u.id == req.params.id);
-    if(user) res.json(user);
+    if (user) res.json(user);
     else next();
   })
-  .patch((req,res,next)=>{
-    const user = users.find((u,i)=>{
-      if(u.id == req.params.id){
-        for(const key in req.body) {
+  .patch((req, res, next) => {
+    const user = users.find((u, i) => {
+      if (u.id == req.params.id) {
+        for (const key in req.body) {
           users[i][key] = req.body[key];
         }
         return true;
       }
     });
 
-    if(user) res.json(user);
+    if (user) res.json(user);
     else next();
   })
-  .delete((req,res,next)=>{
-    const user = users.find((u,i)=>{
-      if(u.id == req.params.id){
-        users.splice(i,1);
+  .delete((req, res, next) => {
+    const user = users.find((u, i) => {
+      if (u.id == req.params.id) {
+        users.splice(i, 1);
         return true;
       }
     });
 
-    if(user) res.json(user);
+    if (user) res.json(user);
     else next();
-  })
+  });
+
 module.exports = router;
