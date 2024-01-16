54863d7d2601afc11c90b8904e7c36774fb3141b
Mindy D Tran <mindytran98@gmail.com>
2024-01-15 21:26:20 -0500

    format doc with prettier

--- utilities/renderPage.js
+++ utilities/renderPage.js
@@ -1,20 +1,20 @@
-function renderPage (arr){
-    let rows =""
-    if(arr.length){
-        arr.forEach(element => {
-            rows+="<tr>"
-            for(const key in element){
-                rows += `<td>${element[key]}</td>`;
-            }
-            rows+="</tr>";
-        });
-    } else {
-        rows+="<tr>"
-            for(const key in arr){
-                rows += `<td>${arr[key]}</td>`;
-            }
-            rows+="</tr>";
+function renderPage(arr) {
+  let rows = "";
+  if (arr.length) {
+    arr.forEach((element) => {
+      rows += "<tr>";
+      for (const key in element) {
+        rows += `<td>${element[key]}</td>`;
+      }
+      rows += "</tr>";
+    });
+  } else {
+    rows += "<tr>";
+    for (const key in arr) {
+      rows += `<td>${arr[key]}</td>`;
     }
-    return rows;
+    rows += "</tr>";
+  }
+  return rows;
 }
 module.exports = renderPage;
