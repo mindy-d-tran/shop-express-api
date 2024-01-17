b85f5988c1a9af04f7409f581fcdacd4e3a64d43
Mindy D Tran <mindytran98@gmail.com>
2024-01-16 21:30:43 -0500

    fix order of parameters

--- utilities/renderPage.js
+++ utilities/renderPage.js
@@ -1,20 +1,22 @@
-const objToTR = (str, obj)=>{
-  str+="<tr>";
+const objToTR = (obj, url) => {
+  let str = "<tr>";
   for (const key in obj) {
-    if(key === "orderList") str += `<td><a href="/api/orders/${obj.id}/orderList">[...]</a></td>`;
-    else str += `<td>${obj[key]}</td>`;
+    if (key === "orderList")
+      str += `<td><a href="/api/${url}/${obj.id}/orderList">[...]</a></td>`;
+    else
+      str += `<td><a href="/api/${url}/${obj.id}">${obj[key]}</a></td>`;
   }
-  str+="</tr>"
+  str += "</tr>";
   return str;
-}
-function renderPage(arr) {
+};
+function renderPage(arr, str) {
   let rows = "";
   if (arr.length) {
     arr.forEach((element) => {
-      rows += objToTR("", element);
+      rows += objToTR(element, str);
     });
   } else {
-    rows += objToTR("", arr);
+    rows += objToTR(arr, str);
   }
   return rows;
 }
