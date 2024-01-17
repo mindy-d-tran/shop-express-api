ba4e14494d0bfc1d8606e3e34ad0214eef7c9285
Mindy D Tran <mindytran98@gmail.com>
2024-01-16 20:20:40 -0500

    fix logic of function

--- utilities/renderPage.js
+++ utilities/renderPage.js
@@ -1,21 +1,20 @@
 const objToTR = (str, obj)=>{
   str+="<tr>";
   for (const key in obj) {
-    if(key === "orderList") str += `<td>[...]</td>`;
+    if(key === "orderList") str += `<td><a href="/api/orders/${obj.id}/orderList">[...]</a></td>`;
     else str += `<td>${obj[key]}</td>`;
   }
   str+="</tr>"
   return str;
 }
-
 function renderPage(arr) {
   let rows = "";
   if (arr.length) {
     arr.forEach((element) => {
-      rows+= objToTR(rows, element)
+      rows += objToTR("", element);
     });
   } else {
-    rows+=objToTR(rows, arr);
+    rows = objToTR(rows, arr);
   }
   return rows;
 }
